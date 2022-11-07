5a94088656a77f14db46e022c74026c339eee46b
Andrzej <andis_as@wp.pl>
2022-11-07 22:08:23 +0100

    after merge

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <h2>App starting... test 5</h2>
-=======
-      <h2>App starting... test 4.2</h2>
->>>>>>> 73a5e50080e4f91c52b5512fa5191edb1ea92a58
+
     </div>
   );
 }
